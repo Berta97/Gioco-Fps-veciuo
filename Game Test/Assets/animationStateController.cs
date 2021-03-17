19fc2372463bd784a89ce921b013e3d9290c3664
Berta97 <davide.bertasini@gmail.com>
2021-03-17 11:18:13 +0100

    modifiche

--- Game Test/Assets/animationStateController.cs
+++ Game Test/Assets/animationStateController.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class animationStateController : MonoBehaviour
 {
+    Animator animator;
+    int IsWalkingHash;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        IsWalkingHash = Animator.StringToHash("IsWalking")
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool IsRunning = animator.GetBool("IsRunning")
+        bool IsWalking = animator.GetBool(isWalkingHash)
+
+
+        if (Input.GetKey("w"))
+        {
+            animator.SetBool("IsWalking", true);
+        }
+        if(!Input.GetKey("w"))
+            {
+            animator.SetBool("IsWalking", false);
+        }
     }
 }
